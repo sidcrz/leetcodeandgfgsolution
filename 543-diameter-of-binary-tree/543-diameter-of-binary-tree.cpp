6ffc57fd88f272b7507aca1b1aa03833cc83170b
Siddhartha Singh Chauhan <57244923+sidcrz@users.noreply.github.com>
2022-08-25 12:25:20 +0530

    Time: 15 ms (65.70%), Space: 20.3 MB (26.76%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,35 +1,30 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int diameterOfBinaryTree(TreeNode *root)
-        {
-            int dim = 0;
-            dia(root, dim);
-            return dim;
-
-        }
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int dl=0;
+        binary(root,dl);
+        return dl;
+        
+        
+    }
     private:
-        int dia(TreeNode *node, int &dim)
-        {
-            if (node == NULL)
-            {
-                return 0;
-            }
-            int lh = dia(node->left, dim);
-            int rh = dia(node->right, dim);
-            dim = max(dim, lh + rh);
-            return 1 + max(lh, rh);
-        }
-
+    int binary(TreeNode* root,int &dl)
+    {
+        if(root==NULL)return 0;
+        int left=binary(root->left,dl);
+        int right=binary(root->right,dl);
+        dl=max(dl,left+right);
+        return 1+max(left,right);
+    }
 };
