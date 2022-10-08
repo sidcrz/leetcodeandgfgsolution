ed76d5d386c21ae37cac53c6756539bc18ee8023
Siddhartha Singh Chauhan <57244923+sidcrz@users.noreply.github.com>
2022-10-08 11:29:13 +0530

    Time: 13 ms (58.38%), Space: 10.7 MB (97.70%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,25 +2,32 @@ class Solution {
     public:
    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds)
    {
-    if(ind==arr.size())
-    {
-        if(target==0)
-        {
-            ans.push_back(ds);
-        }
-            return;
-    }
-       //pick the elements
-       if(arr[ind]<=target)
+   if(ind==arr.size())
+   {
+       if(target==0)
        {
-           ds.push_back(arr[ind]);
-           findCombination(ind,target-arr[ind],arr,ans,ds);
-           ds.pop_back();
-            }
-       //not to pick
+           ans.push_back(ds);
+       }
+       return ;
+   }
+      if(arr[ind]<=target)
+      {
+          ds.push_back(arr[ind]);
+    
+       findCombination(ind,target-arr[ind],arr,ans,ds);
+       
+       ds.pop_back();
+      }
        findCombination(ind+1,target,arr,ans,ds);
-}
-public:
+       
+       
+   }
+    
+       
+       
+       
+       
+
 vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
     vector<vector<int>> ans;
     vector<int> ds;
