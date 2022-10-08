488b26a3638e2179774be74e7151b8080d2c310a
Siddhartha Singh Chauhan <57244923+sidcrz@users.noreply.github.com>
2022-10-08 14:40:51 +0530

    Time: 10 ms (59.56%), Space: 10.6 MB (73.24%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -9,23 +9,20 @@ public:
     }
      void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds)
    {
-   
-        if(target==0)
-        {
-            ans.push_back(ds);
-            return;
+  
+       if(target==0)
+       {
+           ans.push_back(ds);
+       return;
         }
-            
-
-       for(int i=ind;i<arr.size();i++)
-       {
-           if(i>ind && arr[i]==arr[i-1]) continue;
-       if(arr[i]>target) break;
-       {
-           ds.push_back(arr[i]);
-           findCombination(i+1,target-arr[i],arr,ans,ds);
-           ds.pop_back();
-            }
-       }
+         
+    for(int i=ind;i<arr.size();i++)
+    {
+        if(i>ind && arr[i]==arr[i-1]) continue;
+        if(target<arr[i]) break;
+        ds.push_back(arr[i]);
+        findCombination(i+1,target-arr[i],arr,ans,ds);
+        ds.pop_back();
+    }
      }
 };
