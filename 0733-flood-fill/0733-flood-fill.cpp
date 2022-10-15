ce08e168a34180888be672d86b4d2df46b4ff529
Siddhartha Singh Chauhan <57244923+sidcrz@users.noreply.github.com>
2022-10-15 16:10:51 +0530

    Time: 20 ms (38.13%), Space: 14.2 MB (26.79%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        queue<pair<int,int>>q;
+        int x=color;
+        int y=image[sr][sc];
+        int col=image[0].size();
+        int row=image.size();
+        if(x==y)
+        {
+            return image;
+        }
+         q.push({sr,sc});
+        
+        while(!q.empty())
+        {
+            int sm=q.front().first;
+            int mn=q.front().second;
+            q.pop();
+            
+            if(sm<0||mn<0||sm>=row||mn>=col)
+            {
+                continue;
+            }
+           // cout<<sm<<" "<<mn<<endl;
+            if(image[sm][mn]==y)
+            {
+                image[sm][mn]=x;
+                q.push({sm,mn+1});
+                q.push({sm,mn-1});
+                q.push({sm-1,mn});
+                q.push({sm+1,mn});
+                
+            }
+            
+        }
+        return image;
+    }
+};
