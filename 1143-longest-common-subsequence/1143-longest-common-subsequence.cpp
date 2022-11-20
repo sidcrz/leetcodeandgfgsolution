21186afd6db20cc5f4d237d7e30953f677ca8210
Siddhartha Singh Chauhan <57244923+sidcrz@users.noreply.github.com>
2022-11-20 21:54:03 +0530

    Time: 80 ms (12.50%), Space: 13.1 MB (24.19%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+    int solution(int index1, int index2, string &X,string&Y,vector<vector<int>>&dp)
+    {
+        if(index1<0||index2<0)
+            return 0;
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        if(X[index1]==Y[index2])
+            return dp[index1][index2]=1+solution(index1-1,index2-1,X,Y,dp);
+        else
+            return dp[index1][index2]= max(solution(index1-1,index2,X,Y,dp),solution(index1,index2-1,X,Y,dp));
+    }
     int longestCommonSubsequence(string X, string Y) {
+        int index1=X.size();
+        int index2=Y.size();
+        vector<vector<int>>dp(index1+1,vector<int>(index2+1,-1));
+        return solution(index1-1,index2-1,X,Y,dp);
         
-        int n = X.length(), m = Y.length();
-        int dp[n + 1][m + 1]; // DP - matrix
-
-	// initialize 1st row and of dp matrix with 0 according to the base condition of recursion // base case of recursion --> for initialization of dp - matrix
-	for (int i = 0; i <= n; i++)
-		for (int j = 0; j <= m; j++)
-			if (i == 0 || j == 0)
-				dp[i][j] = 0;
-// choise diagram is used to fill rest of the matrix 
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= m; j++)
-			if (X[i - 1] == Y[j - 1]) // when last character is same
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			else // when last character is not same -> pick max
-				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-
-	return dp[n][m]; // last row and last column element will give the length of the LCS;
-}
+        
+    }
+    
+    
 };
