0ce04bcc465362e6891061113a8f380aad71df4e
Siddhartha Singh Chauhan <57244923+sidcrz@users.noreply.github.com>
2022-11-25 12:55:49 +0530

    Time: 649 ms (37.92%), Space: 116.7 MB (75.66%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,65 +1,61 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* reverse(ListNode* head)
-    {
-        ListNode* newhead=NULL;
-        while(head!=NULL)
+class Solution
+{
+    public:
+        ListNode* reverse(ListNode *head)
         {
-            ListNode* next=head->next;
-            head->next=newhead;
-            newhead=head;
-            head=next;
+            ListNode *newhead = NULL;
+            while (head != NULL)
+            {
+                ListNode *next = head->next;
+                head->next = newhead;
+                newhead = head;
+                head = next;
+            }
+            return newhead;
         }
-        return newhead;
-    }
     
-    
-    ListNode* middle(ListNode* head)
+
+    ListNode* middle(ListNode *head)
     {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL)
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
         {
-            fast=fast->next->next;
-            slow=slow->next;
+            fast = fast->next->next;
+            slow = slow->next;
         }
         return slow;
     }
-    int pairSum(ListNode* head) 
+    int pairSum(ListNode *head)
     {
-        ListNode* first=head;
-        ListNode* second = middle(head);
-        ListNode* third =reverse(second);
-        int maxi=INT_MIN;
-    
-        while(third!=NULL)
+        //for example our linked list is [1 2 3 4]
+        ListNode *first = head;
+        //find the middle of linked list using slow and fast pointer 
+        //middle is 3 
+        ListNode *second = middle(head);
+        //we put 3 as head and reverse it now the linked list is [1,2,4,3]// 
+        ListNode *third = reverse(second);
+        
+        
+        int maxi = INT_MIN; 
+
+        while (third != NULL)
         {
-            int sum=(third->val)+(first->val);
-            maxi=max(sum,maxi);
-            third=third->next;
-            first=first->next;
-            
-            
-            
+            int sum = (third->val) + (first->val);
+            maxi = max(sum, maxi);
+            third = third->next;
+            first = first->next;
         }
         return maxi;
-        
-        
-        
-        
-        
-        
-        
     }
-    
 };
